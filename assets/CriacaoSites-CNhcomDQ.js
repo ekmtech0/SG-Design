4180b4e395e814e8c272f5c70c8979acce10e3c6
Edvaldo Dantas <135360521+EdvaldoDantas@users.noreply.github.com>
2025-10-16 22:43:45 +0100

    Updates

---
+++ assets/CriacaoSites-CNhcomDQ.js
@@ -0,0 +1,1 @@
+import{N as a}from"./NavBar-BZhUGLEJ.js";import{c as r,b as t,a as o,F as s,o as n}from"./index-CvlwwXR8.js";const p={__name:"CriacaoSites",setup(c){return(i,e)=>(n(),r(s,null,[t(a),e[0]||(e[0]=o("div",null,"Criação de Sites",-1))],64))}};export{p as default};
